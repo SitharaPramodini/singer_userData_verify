fdf1995ee04e013c758ffcb5c0baadbf99137ed5
SitharaPramodini <it21306754@my.sliit.lk>
2025-07-16 12:05:51 +0530

    barcode

--- src/components/Barcode.jsx
+++ src/components/Barcode.jsx
@@ -1,6 +1,21 @@
-import { BarcodeScanner } from 'react-barcode-scanner';
-import 'react-barcode-scanner/polyfill';
+import { useState } from 'react';
+import useScanDetection from "use-scan-detection";
 
-export default function Barcode() {
-  return <BarcodeScanner />;
+function Barcode() {
+
+    const [barcodeScan, setBarcodeScan] = useState("No Barcode Scanned");
+
+    useScanDetection({
+        onComplete: setBarcodeScan,
+        minLength: 3,
+    })
+    return (
+        <div className="App" style={{ padding: "5rem 10rem" }}>
+            <p>
+                Barcode: {barcodeScan}
+            </p>
+        </div >
+    );
 }
+
+export default Barcode;
